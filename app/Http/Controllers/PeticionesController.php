d5c108c6e73be1e76369a6c0c376e0904d45c9a4
AlejandroMartinez101 <daw112.2023@gmail.com>
2024-03-04 20:23:34 +0100

    laravel_api_Ale2

--- app/Http/Controllers/PeticionesController.php
+++ app/Http/Controllers/PeticionesController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use App\Models\Peticione;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PeticionesController extends Controller
 {
@@ -18,12 +20,12 @@ class PeticionesController extends Controller
     }
     public function listMine(Request $request)
     {
-// parent::index()
-//$user = Auth::user();
-        $id = 1;
-        $peticiones = Peticione::all()->where('user_id', $id);
-return $peticiones;
-}
+        // parent::index()
+        $user = Auth::user();
+        //$id = 1;
+        $peticiones = Peticione::all()->where('user_id', $user->id);
+        return $peticiones;
+    }
     public function show(Request $request, $id)
     {
         $peticion = Peticione::findOrFail($id);
@@ -42,33 +44,36 @@ return $peticiones;
             'descripcion' => 'required',
             'destinatario' => 'required',
             'categoria_id' => 'required',
-// 'file' => 'required',
+            // 'file' => 'required',
         ]);
         $input = $request->all();
         $category = Categoria::findOrFail($request->input('categoria_id'));
-        $user=1; //harcodeamos el usuario
-//$user = Auth::user(); //asociarlo al usuario authenticado
+        //$user=1; //harcodeamos el usuario
+        $user = Auth::user(); //asociarlo al usuario authenticado
         $peticion = new Peticione($input);
         $peticion->user()->associate($user);
-$peticion->categoria()->associate($category);
-$peticion->firmantes = 0;
-$peticion->estado = 'pendiente';
-$peticion->save();
-return $peticion;
-}
+        $peticion->categoria()->associate($category);
+        $peticion->firmantes = 0;
+        $peticion->estado = 'pendiente';
+        $peticion->save();
+        $res = $peticion;
+        if($res){
+            return response() ->json(['message' => 'Peticion creada satisfactoriamente', 'peticion' => $peticion],201);
+        }
+        return  response() -> json (['message' => 'Error creando la peticion'], 500);
+    }
     public function firmar(Request $request, $id)
     {
         $peticion = Peticione::findOrFail($id);
-//$user = Auth::user();
-        $user = 1;
-        $user_id = [$user];
-//$user_id = [$user‐>id];
-
-$peticion->firmas()->attach($user_id);
-$peticion->firmantes = $peticion->firmantes + 1;
-$peticion->save();
-return $peticion;
-}
+        $user = Auth::user();
+        //$user = 1;
+        //$user_id = [$user];
+        $user_id = [$user->id];
+        $peticion->firmas()->attach($user_id);
+        $peticion->firmantes = $peticion->firmantes + 1;
+        $peticion->save();
+        return $peticion;
+    }
     public function cambiarEstado(Request $request, $id)
     {
         $peticion = Peticione::findOrFail($id);
@@ -83,4 +88,3 @@ return $peticion;
         return $peticion;
     }
 }
-
